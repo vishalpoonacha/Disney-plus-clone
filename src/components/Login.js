dc97891d0560bfbeb0a56a789188d4196c5133bc
vishalpoonacha007@gmail.com <vishalpoonacha007@gmail.com>
2021-04-25 00:10:38 +0530

    redux

---
+++ src/components/Login.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Login =(props) =>{
+    return(
+    <Container>
+        <Content>
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.png"/>
+            </CTA>
+            <BgImage/>
+            </Content>
+    </Container>
+    )
+    
+}
+
+const Container = styled.section`
+    overflow:hidden;
+    display:flex;
+    flex-direction:column;
+    text-align:center;
+    height:100vh;
+
+`;
+
+const Content = styled.div`
+    margin-bottom: 10vw;
+    width:100%;
+    position:relative;
+    min-height:100vh;
+    box-sizing:border-box;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+    padding:80px 40px;
+    height:100%;
+
+`;
+
+const BgImage = styled.div`
+    height:100%;
+    background-position:top;
+    background-repeat: no-repeat;
+    background-size:cover;
+    background-image:url('/images/login-background.jpg');
+    position:absolute;
+    top:0;
+    right:0;
+    left:0;
+    z-index:-1;
+`;
+
+
+const CTA = styled.div``;
+
+const CTALogoOne = styled.img`
+    margin-bottom:12px;
+    max-width:600px;
+    min-height:1px;
+    display:block;
+    width:100%;
+
+`;
+
+
+export default Login
